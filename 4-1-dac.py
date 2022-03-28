0a10ac70363d46aef7a24afcef9c81afc1c5c62b
kravetsma <101568655+kravetsma@users.noreply.github.com>
2022-03-28 18:19:14 +0300

    Add files via upload

--- 4-1-dac.py
+++ 4-1-dac.py
@@ -1,32 +1,21 @@
-from RPi import GPIO as gpio
+import RPi.GPIO as gpio
 
 
-def cycle():
-    num = input("Введите число от 0 до 100:\n")
-    if num == 'q':
-        return None
-    if not num.isdigit():
-        print('input error')
-        return cycle()
-    if abs(int(num)) != int(num):
-        print('input error')
-        return cycle()
-    if int(num) > 100:
-        print('input error')
-        return cycle()
-    return int(num)
-
+def decimal2binary(value): 
+    return [int(bit) for bit in bin(value)[2:].zfill(8)]
 
 gpio.setmode(gpio.BCM)
-gpio.setup(22, gpio.OUT)
-shim = gpio.PWM(22, 1000)
-shim.start(0)
-
+pinNum = 23
+gpio.setup(pinNum,gpio.OUT)
+pwm = gpio.PWM(pinNum,1000)
+pwm.start(0)
 try:
-    while __name__ == '__main__':
-        num = cycle()
-        shim.ChangeDutyCycle(num)
-        print("approx. voltage: {:.4f}".format(3.3 * num / 100))
+    num = input()
+    while num!='q':
+        num = int(num)
+        pwm.start(num)
+        num = input()
 finally:
-    gpio.output(22, 0)
+    pwm.stop()
+    gpio.output(pinNum,0)
     gpio.cleanup()
